65b1282ae51ac7d04f27ec29b8aa803752c20207
aniketh <sunnyrockzzz1811@gmail.com>
2025-05-23 13:24:53 +0530

    Add CORS

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,12 @@ import express, { urlencoded } from "express"
 import authRouter from "./routers/authRouter.js"
 import postRouter from "./routers/postRouter.js"
 import authMiddleware from "./middlewares/authMiddleware.js"
+import cors from "cors"
 
 const app = express()
 const PORT = process.env.PORT || 3000
 app.use(urlencoded({ extended: true }))
+app.use(cors)
 
 app.get("/", authMiddleware, (req, res) => res.send({user: req.user}))
 app.use("/", authRouter)
