fe91d1cb31a74b2dd767903c74636e79f31a9cbe
aniketh <sunnyrockzzz1811@gmail.com>
2025-05-24 15:01:03 +0530

    Add comments

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -1,11 +1,33 @@
+import { useState } from "react";
+import apiClient from "../apiClient";
+
 function Post({ post }) {
-    
+    const [comment, setComment] =useState("")
+
+    async function handleEnterCommentButton(){
+        await apiClient.post("/"+post.id+"/comment", {content: comment}, 
+            {
+                    headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+            }
+        )
+
+        window.location.reload()
+    }
+
     return ( <div style={{border: "2px solid red"}}>
     <h1>{post.authorUsername}</h1>
     <p>{post.content}</p>
     <div>
         <span>Likes: {post.likes.length}</span>
-        <span> Comments: {post.comments.length}</span>
+        <input type="text" placeholder="add your comment" value={comment} onChange={(e)=>setComment(e.target.value)}/>
+        <button onClick={handleEnterCommentButton}>Add Comment</button>
+        <div style={{border: "1px solid green"}}> 
+            {post.comments.map((comment)=>{
+            return(<>
+            <h5> {comment.authorUsername} </h5>
+            <span>{comment.content}</span>
+            </>)
+            })}</div>
     </div>
     </div> );
 }
