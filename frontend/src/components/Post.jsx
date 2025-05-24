07267762142010d676537ab64b0ff61618839e50
aniketh <sunnyrockzzz1811@gmail.com>
2025-05-24 15:13:30 +0530

    Add Like

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -2,34 +2,46 @@ import { useState } from "react";
 import apiClient from "../apiClient";
 
 function Post({ post }) {
-    const [comment, setComment] =useState("")
+    const [comment, setComment] = useState("")
 
-    async function handleEnterCommentButton(){
-        await apiClient.post("/"+post.id+"/comment", {content: comment}, 
+    async function handleEnterCommentButton() {
+        await apiClient.post("/" + post.id + "/comment", { content: comment },
             {
-                    headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
             }
         )
 
         window.location.reload()
     }
 
-    return ( <div style={{border: "2px solid red"}}>
-    <h1>{post.authorUsername}</h1>
-    <p>{post.content}</p>
-    <div>
-        <span>Likes: {post.likes.length}</span>
-        <input type="text" placeholder="add your comment" value={comment} onChange={(e)=>setComment(e.target.value)}/>
-        <button onClick={handleEnterCommentButton}>Add Comment</button>
-        <div style={{border: "1px solid green"}}> 
-            {post.comments.map((comment)=>{
-            return(<>
-            <h5> {comment.authorUsername} </h5>
-            <span>{comment.content}</span>
-            </>)
-            })}</div>
-    </div>
-    </div> );
+    async function handleLikePostButton(){
+        await apiClient.post("/" + post.id + "/like")
+
+        window.location.reload()
+    }
+
+    return (<div style={{ border: "2px solid red" }}>
+        <h1>{post.authorUsername}</h1>
+        <p>{post.content}</p>
+        <div>
+            <span onClick={()=>document.querySelector('dialog').showModal()}>Likes: {post.likes.length}</span>
+            <dialog>
+                <div>{post.likes.map((like)=><div key={like.id}>{like.username}</div>)}</div>
+                <button onClick={()=>document.querySelector('dialog').close()}>close!</button>
+            </dialog>
+            <button onClick={handleLikePostButton}>Like</button>
+
+            <input type="text" placeholder="add your comment" value={comment} onChange={(e) => setComment(e.target.value)} />
+            <button onClick={handleEnterCommentButton}>Add Comment</button>
+            <div style={{ border: "1px solid green" }}>
+                {post.comments.map((comment) => {
+                    return (<div key={comment.id}>
+                        <h5> {comment.authorUsername} </h5>
+                        <span>{comment.content}</span>
+                    </div>)
+                })}</div>
+        </div>
+    </div>);
 }
 
 export default Post;
