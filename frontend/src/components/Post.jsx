b8c0b294a28fc30da6bf5469ed027da2b6af2c7d
aniketh <sunnyrockzzz1811@gmail.com>
2025-05-27 11:04:17 +0530

    Bug Fix

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import apiClient from "../apiClient";
 
 function Post({ post }) {
     const [comment, setComment] = useState("")
+    const dilogRef = useRef(null)
 
     async function handleEnterCommentButton() {
         await apiClient.post("/" + post.id + "/comment", { content: comment },
@@ -24,18 +25,18 @@ function Post({ post }) {
         <h1>{post.authorUsername}</h1>
         <p>{post.content}</p>
         <div>
-            <span onClick={()=>document.querySelector('dialog').showModal()}>Likes: {post.likes.length}</span>
-            <dialog>
-                <div>{post.likes.map((like)=><div key={like.id}>{like.username}</div>)}</div>
-                <button onClick={()=>document.querySelector('dialog').close()}>close!</button>
+            <span onClick={()=>dilogRef.current.showModal()}>Likes: {post.likes.length}</span>
+            <dialog ref={dilogRef}>
+                <div>{post.likes.map((like,index)=><div key={index}>{like.username}</div>)}</div>
+                <button onClick={()=>dilogRef.current.close()}>close!</button>
             </dialog>
             <button onClick={handleLikePostButton}>Like</button>
 
             <input type="text" placeholder="add your comment" value={comment} onChange={(e) => setComment(e.target.value)} />
             <button onClick={handleEnterCommentButton}>Add Comment</button>
             <div style={{ border: "1px solid green" }}>
-                {post.comments.map((comment) => {
-                    return (<div key={comment.id}>
+                {post.comments.map((comment,index) => {
+                    return (<div key={index}>
                         <h5> {comment.authorUsername} </h5>
                         <span>{comment.content}</span>
                     </div>)
